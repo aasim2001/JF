8a6e8eaa78be841f9cc3ae0664dd46031e9712b5
Aasim Bairagdar <aasim2001@gmail.com>
2024-03-02 22:07:25 +0530

    api 1

--- h.php
+++ h.php
@@ -1,9 +1,14 @@
 <?php
 $headers = apache_request_headers();
-$headerName = 'Connection';
+$headerName = 'toekn';
 
 if(array_key_exists($headerName, $headers)) {
-    echo $headers[$headerName];
+    if($headers[$headerName] == 'ABCD1234') {
+
+    } else {
+        echo 'Unauthorized Access'
+    }
+    
 } else {
     $url = 'https://tinypng.com/';
     header("Location: https://www.w3schools.com/php/func_array_push.asp");
--- index.php
+++ index.php
@@ -1,42 +1,56 @@
 <?php
-
+$headers = apache_request_headers();
+$headerName = 'toekn';
 header('Content-Type: application/json; charset=utf-8');
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "JF";
+if(array_key_exists($headerName, $headers)) {
+    if($headers[$headerName] == 'ABCD1234') {
+      $servername = "localhost:3309";
+      $username = "root";
+      $password = "";
+      $dbname = "JF";
+      
+      
+      // Create connection
+      $conn = new mysqli($servername, $username, $password, $dbname);
+      
+      // Check connection
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+      
+      
+      $sql = "SELECT * FROM cameraupload WHERE id=1";
+      $result = $conn->query($sql);
+      $myObj = new stdClass();
+      if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+         // echo "id: " . $row["id"]. " - Name: " . $row["base64"]. "<br>";
+         $myObj->id = $row["id"];
+         $myObj->base64 = $row["base64"];
+         
+        }
+      } else {
+        echo "0 results";
+      }
+      
+      $myJSON = json_encode($myObj);
+      echo $myJSON;
+      $conn->close();
 
-$name = $_POST['name'];
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-
-$sql = "SELECT * FROM cameraupload WHERE id=1";
-$result = $conn->query($sql);
-$myObj = new stdClass();
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-   // echo "id: " . $row["id"]. " - Name: " . $row["base64"]. "<br>";
-   $myObj->id = $row["id"];
-   $myObj->name = $name;
-   $myObj->base64 = $row["base64"];
-   
-  }
+    } else {
+        echo 'Unauthorized Access';
+    }
+    
 } else {
-  echo "0 results";
+    $url = 'https://tinypng.com/';
+    header("Location: https://www.w3schools.com/php/func_array_push.asp");
+    exit();
 }
 
-$myJSON = json_encode($myObj);
-echo $myJSON;
-$conn->close();
+
+
 
 ?>
 
